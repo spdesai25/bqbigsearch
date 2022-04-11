1ab20b108282aeca895f2dcada20983a8272873a
Siddharth Desai <spdesai@google.com>
2022-04-11 22:00:58 +0000

    to create a terraform file

---
+++ main.tf
@@ -0,0 +1,11 @@
+//define a variable with list of GCP services to be enabled
+variable "gcp_service_list" {
+  description ="The list of apis necessary for the demo"
+  type = list(string)
+  default = ["bigquery.googleapis.com",
+            "bigqueryconnection.googleapis.com",
+            "bigquerystorage.googleapis.com",
+            "storage-component.googleapis.com",
+            "storage-api.googleapis.com",
+            "dataproc.googleapis.com"]
+}
